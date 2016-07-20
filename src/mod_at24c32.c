6e9fbdb9a1dcd1e8bea86b35f9f8cb934a87117f
Alex Best <theamp@gmail.com>
2016-07-20 01:30:46 -0500

    Added Peripheral Inputs to functions in code file.

--- src/mod_at24c32.c
+++ src/mod_at24c32.c
@@ -1,1 +1,43 @@
 #include "mod_at24c32.h"
+
+
+uint8_t at24c32_writeByte(I2C_TypeDef* I2Cx, uint8_t toWrite, uint16_t addr);
+
+uint8_t at24c32_writeByteMulti(I2C_TypeDef* I2Cx, 
+        uint8_t* toWrite, 
+        uint16_t addr, 
+        uint8_t len);
+
+uint8_t at24c32_readByte(I2C_TypeDef* I2Cx, uint16_t addr);
+
+uint8_t* at24c32_readByteMulti(I2C_TypeDef* I2Cx, 
+        uint16_t addr, 
+        uint8_t len);
+
+uint8_t at24c32_write16bit(I2C_TypeDef* I2Cx, 
+        uint16_t toWrite, 
+        uint16_t addr); 
+
+uint8_t at24c32_write16bitMulti(I2C_TypeDef* I2Cx, 
+        uint16_t* toWrite, 
+        uint16_t addr, 
+        uint8_t len); 
+
+uint16_t at24c32_read16bit(I2C_TypeDef* I2Cx, uint16_t addr); 
+uint16_t* at24c32_read16bitMulti(I2C_TypeDef* I2Cx, 
+        uint16_t addr, 
+        uint8_t len); 
+
+uint16_t at24c32_write32bit(I2C_TypeDef* I2Cx, 
+        uint32_t toWrite, 
+        uint16_t addr); 
+
+uint16_t at24c32_write32bitMulti(I2C_TypeDef* I2Cx, 
+        uint32_t* toWrite, 
+        uint16_t addr, 
+        uint8_t len); 
+
+uint32_t at24c32_read32bit(I2C_TypeDef* I2Cx, uint16_t addr); 
+uint32_t* at24c32_read32bitMulti(I2C_TypeDef* I2Cx, 
+        uint16_t addr, 
+        uint8_t len); 
