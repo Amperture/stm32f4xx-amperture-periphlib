91473d49ab5b220a5ef9d165a677defeb39c1e3a
Alex Best <theamp@gmail.com>
2016-07-20 01:29:18 -0500

    Added I2C peripheral inputs to functions.

--- inc/mod_at24c32.h
+++ inc/mod_at24c32.h
@@ -15,38 +15,57 @@
 /* Write Single Byte to given 12-bit address
  *     High bits of address are DONTCARES
  */
-uint8_t at24c32_writeByte(uint8_t toWrite, uint16_t addr);
+uint8_t at24c32_writeByte(I2C_TypeDef* I2Cx, uint8_t toWrite, uint16_t addr);
 
 /* Write Multiple Bytes sequentially to 12-bit address.
  *     High bits of address are DONTCARES
  *     Length cannot exceed 32. 
  */
-uint8_t at24c32_writeByteMulti(uint8_t* toWrite, uint16_t addr, uint8_t len);
+uint8_t at24c32_writeByteMulti(I2C_TypeDef* I2Cx, 
+        uint8_t* toWrite, 
+        uint16_t addr, 
+        uint8_t len);
 
 /* Read Single Byte from supplied 12-bit address.
  *     High bits of address are DONTCARES
  */
-uint8_t at24c32_readByte(uint16_t addr);
+uint8_t at24c32_readByte(I2C_TypeDef* I2Cx, uint16_t addr);
 
 /* Read Multiple Bytes from supplied 12-bit address.
  *     High bits of address are DONTCARES
  *     No Max Read Limit
  */
-uint8_t* at24c32_readByteMulti(uint16_t addr, uint8_t len);
+uint8_t* at24c32_readByteMulti(I2C_TypeDef* I2Cx, 
+        uint16_t addr, 
+        uint8_t len);
 
-uint8_t at24c32_write16bit(uint16_t toWrite, uint16_t addr); 
-uint8_t at24c32_write16bitMulti(uint16_t* toWrite, uint16_t addr, 
+uint8_t at24c32_write16bit(I2C_TypeDef* I2Cx, 
+        uint16_t toWrite, 
+        uint16_t addr); 
+
+uint8_t at24c32_write16bitMulti(I2C_TypeDef* I2Cx, 
+        uint16_t* toWrite, 
+        uint16_t addr, 
         uint8_t len); 
 
-uint16_t at24c32_read16bit(uint16_t addr); 
-uint16_t* at24c32_read16bitMulti(uint16_t addr, uint8_t len); 
-
-uint16_t at24c32_write32bit(uint32_t toWrite, uint16_t addr); 
-uint16_t at24c32_write32bitMulti(uint32_t* toWrite, uint16_t addr, 
+uint16_t at24c32_read16bit(I2C_TypeDef* I2Cx, uint16_t addr); 
+uint16_t* at24c32_read16bitMulti(I2C_TypeDef* I2Cx, 
+        uint16_t addr, 
         uint8_t len); 
 
-uint32_t at24c32_read32bit(uint16_t addr); 
-uint32_t* at24c32_read32bitMulti(uint16_t addr, uint8_t len); 
+uint16_t at24c32_write32bit(I2C_TypeDef* I2Cx, 
+        uint32_t toWrite, 
+        uint16_t addr); 
+
+uint16_t at24c32_write32bitMulti(I2C_TypeDef* I2Cx, 
+        uint32_t* toWrite, 
+        uint16_t addr, 
+        uint8_t len); 
+
+uint32_t at24c32_read32bit(I2C_TypeDef* I2Cx, uint16_t addr); 
+uint32_t* at24c32_read32bitMulti(I2C_TypeDef* I2Cx, 
+        uint16_t addr, 
+        uint8_t len); 
 
 #endif //_AMP_AT24C32_H
 
