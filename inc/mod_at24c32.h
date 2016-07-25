4aba1cdd614bc999fc5778bf65fa3e3f82ed598a
Alex Best <theamp@gmail.com>
2016-07-24 20:12:16 -0500

    Doxygen comments added to mod_at24c32

--- inc/mod_at24c32.h
+++ inc/mod_at24c32.h
@@ -18,29 +18,46 @@
 #define AT24C32_MAX_BUS_TIME 1716
 #endif
 
-/* Write Single Byte to given 12-bit address
- *     High bits of address are DONTCARES
+/* Write Single Byte
+ * @brief Write a Single Byte to the AT24C32 device on bus.
+ *
+ * @param *I2Cx: Which I2C peripheral AT24C32 is attached to.
+ * @param toWrite: 8-bit value to write onto device.
+ * @param addr: Address of memory location desired to write to.
  */
 uint8_t at24c32_writeByte(I2C_TypeDef* I2Cx, uint8_t toWrite, uint16_t addr);
 
-/* Write Multiple Bytes sequentially to 12-bit address.
- *     High bits of address are DONTCARES
- *     Length cannot exceed 32. 
+/* Write Multiple Bytes
+ * @brief Write a string of 8-bit values to the device.
+ * Please note that the high bits of the \c addr are \c DONTCARE
+ * Please note that \c len  should not exceed 32
+ *
+ * @param *I2Cx: Which I2C peripheral AT24C32 is attached to.
+ * @param *toWrite: Pointer to array of 8-bit values.
+ * @param addr: First address in memory of device to write to.
+ * @param len: Length of data array to write to device
  */
 uint8_t at24c32_writeByteMulti(I2C_TypeDef* I2Cx, 
         uint8_t* toWrite, 
         uint16_t addr, 
         uint8_t len);
 
-/* Read Single Byte from supplied 12-bit address.
- *     High bits of address are DONTCARES
+/* Read Single Byte
+ * @brief Read a single 8-bit value from the device.
+ * Please note that the high bits of the \c addr are \c DONTCARE
+ *
+ * @param *I2Cx: Which I2C peripheral AT24C32 is attached to.
+ * @param addr: Memory address on device to pull byte from.
+ *
+ * @retval System returns the contents of the memory location.
  */
 uint8_t at24c32_readByte(I2C_TypeDef* I2Cx, uint16_t addr);
 
+/* EVERYTHING BELOW THIS LINE CONSIDER WIP.
+ *
 /* Read Multiple Bytes from supplied 12-bit address.
  *     High bits of address are DONTCARES
  *     No Max Read Limit
- */
 uint8_t* at24c32_readByteMulti(I2C_TypeDef* I2Cx, 
         uint16_t addr, 
         uint8_t len);
@@ -72,6 +89,7 @@ uint32_t at24c32_read32bit(I2C_TypeDef* I2Cx, uint16_t addr);
 uint32_t* at24c32_read32bitMulti(I2C_TypeDef* I2Cx, 
         uint16_t addr, 
         uint8_t len); 
+*/
 
 #endif //_AMP_AT24C32_H
 
