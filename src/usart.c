ff09c70a21d112647a84ac67f43d9263d5808ff7
Alex Best <theamp@gmail.com>
2016-07-24 19:11:18 -0500

    Moved Doxygen comments to Header File instead of code file.

--- src/usart.c
+++ src/usart.c
@@ -22,19 +22,6 @@
 #include <stdint.h>
 #include <usart.h>
 
-/** USART Init
- * @brief Init function for USART peripheral.
- * @param *USARTx: Which USART internal peripheral to use.
- * @param *GPIOx: Which GPIO Port to use.
- * @param txPin: GPIO Pin to use for Tx, send as integer, NOT Bitmask.
- * @param rxPin: GPIO Pin to use for Rx, send as integer, NOT Bitmask.
- * @param ctsPin: GPIO Pin to use for CTS, send as integer, NOT Bitmask.
- * @param rtsPin: GPIO Pin to use for RTS, send as integer, NOT Bitmask.
- * @param ckPin: GPIO Pin to use for CK, send as integer, NOT Bitmask.
- * @param afMode: Alternate Function mode for GPIO pins, refer to datasheet.
- * @retval void
- * @see http://www.st.com/st-web-ui/static/active/en/resource/technical/document/datasheet/DM00102166.pdf
- */
 void usartInit(USART_TypeDef* USARTx, GPIO_TypeDef* GPIOx, uint8_t txPin,
         uint8_t rxPin, uint8_t ctsPin, uint8_t rtsPin, uint8_t ckPin,
         uint8_t afMode){
@@ -162,11 +149,6 @@ void usartInit(USART_TypeDef* USARTx, GPIO_TypeDef* GPIOx, uint8_t txPin,
     else GPIOx -> AFR[0] |= (afMode << (4 * rtsPin));
 }
 
-/** USART Receive Single Byte
- * @brief Pulls a single byte over the configured USART peripheral.
- * @param *USARTx: Which USART peripheral to send data over.
- * @retval 8-bit data pulled from USART.
- */
 uint8_t usartByteReceive(USART_TypeDef* USARTx){
     uint8_t data = 0;
     while( !((USARTx -> SR) & USART_SR_RXNE));
@@ -174,21 +156,11 @@ uint8_t usartByteReceive(USART_TypeDef* USARTx){
     return data;
 }
 
-/** USART Send Single Byte
- * @brief Sends a single byte over the configured USART peripheral.
- * @param *USARTx: Which USART peripheral to send data over.
- * @param data: Byte to send.
- */
 void usartByteSend(USART_TypeDef* USARTx, uint8_t data){
     while( !(USARTx -> SR & USART_SR_TXE));
     USARTx -> DR = data;
 }
 
-/** USART Send String 
- * @brief Send a string of data in 8-bit chunks over UART.
- * @param USARTx: Which USART peripheral to send data over.
- * @param data: Pointer to **null-terminated** data set. 
- */
 void usartStringSend(USART_TypeDef* USARTx, uint8_t* data){
     while(*data){
         usartByteSend(USARTx, *(data++));
